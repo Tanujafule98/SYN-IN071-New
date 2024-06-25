0dc83cd984d04ad6327f9633ad38bf060821a487
Tanuja Fule <tanujafule98@gmail.com>
2024-06-25 15:14:27 +0530

    edit the code

--- prime.py.py
+++ prime.py.py
@@ -1,10 +1,4 @@
-'''
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 num = 1
 # If given number is greater than 1
 if num > 1:
